0f6a71bebc5d6e2fd6f2b9c4c78c24590bf4733c
Anonymeoww <47354277+Anonymeoww@users.noreply.github.com>
2019-05-29 15:31:42 +0200

    graphs are linked, only styling and ordering the bars

--- Homework/Week_6/linked.js
+++ Homework/Week_6/linked.js
@@ -1,164 +1,184 @@
-d3v5.json("data.json").then(function(data){
+d3v5.json("data.json").then(function(all_data){
+    console.log(all_data);
 
-  EUcountries = ['AUT', 'BEL', 'BG', 'BGR', 'HR', 'HRV', 'CYP', 'CZE', 'DNK', 'EST', 'FIN', 'FRA', 'DEU', 'GRC', 'HUN',
-      'IRL', 'ITA', 'LVA', 'LTU', 'LUX', 'MLT', 'NLD', 'POL', 'PRT', 'ROU', 'SVK','SVN', 'ESP', 'SWE', 'GBR'];
+    var map_data = format_data(all_data);
+    create_map(map_data);
+    create_bars(map_data, "NLD")
+});
 
-  NAcountries = ['ATG', 'ABW', 'BHS', 'BRB', 'BLZ', 'BMU', 'BES', 'VGB', 'CAN', 'CYM', 'CRI', 'CUB', 'CUW','DMA', 'DOM',
-       'SLV', 'GRL', 'GRD', 'GLP', 'GTM', 'HTI', 'HND', 'JAM', 'MTQ', 'MEX', 'SPM', 'MSR', 'ANT', 'KNA', 'NIC', 'PAN',
-       'PRI', 'BES', 'SXM', 'LCA', 'SPM', 'VCT', 'TTO', 'TCA', 'USA', 'VIR'];
+function format_data(data) {
+    EUcountries = ['AUT', 'BEL', 'BG', 'BGR', 'HR', 'HRV', 'CYP', 'CZE', 'DNK', 'EST', 'FIN', 'FRA', 'DEU', 'GRC', 'HUN',
+        'IRL', 'ITA', 'LVA', 'LTU', 'LUX', 'MLT', 'NLD', 'POL', 'PRT', 'RUS', 'ROU', 'SVK', 'SVN', 'ESP', 'SWE', 'GBR'];
 
-  EUcountries.forEach(function(euCountry){
-    data["EU"][euCountry] = {
-      Total: data["EU"].Total,
-      Genres: data["EU"]["Genres"]
-    }
-  });
+    NAcountries = ['ATG', 'ABW', 'BHS', 'BRB', 'BLZ', 'BMU', 'BES', 'VGB', 'CAN', 'CYM', 'CRI', 'CUB', 'CUW', 'DMA', 'DOM',
+        'SLV', 'GRL', 'GRD', 'GLP', 'GTM', 'HTI', 'HND', 'JAM', 'MTQ', 'MEX', 'SPM', 'MSR', 'ANT', 'KNA', 'NIC', 'PAN',
+        'PRI', 'BES', 'SXM', 'LCA', 'SPM', 'VCT', 'TTO', 'TCA', 'USA', 'VIR'];
 
-  NAcountries.forEach(function(naCountry){
-    data["USA"][naCountry] = {
-      Total: data["USA"].Total
-    }
-  });
+    Othercountries = ['AFG', 'ARM', 'AZE', 'BHR', 'BGD', 'BTN', 'BRN', 'KHM', 'CHN', 'CXR', 'CCK', 'IOT', 'GEO', 'HKG',
+        'IND', 'IDN', 'IRN', 'IRQ', 'ISR', 'JOR', 'KAZ', 'KWT', 'KGZ', 'LAO', 'LBN', 'MAC', 'MYS', 'MDV', 'MNG', 'MMR',
+        'NPL', 'PRK', 'OMN', 'PAK', 'PSE', 'PHL', 'QAT', 'SAU', 'SGP', 'KOR', 'LKA', 'SYR', 'TWN', 'TJK', 'THA', 'TUR',
+        'TKM', 'ARE', 'UZB', 'VNM', 'YEM'];
 
-  // var onlyValues = [];
-  //
-  // Object.keys(data).forEach(function (a) {
-  //   onlyValues.push(data[a]["Total"]);
-  //   return (data[a]["Total"])
-  // });
-  // console.log(onlyValues)
-  //
-  // // colors should be uniq for every value.
-  // // For this purpose we create palette(using min/max series-value)
-  // var minValue = Math.min.apply(null, onlyValues),
-  //       maxValue = Math.max.apply(null, onlyValues);
+    var country_dict = {};
 
-  // create color palette function
-  // color can be whatever you wish
-  var paletteScale = function    (color){
-        if (color > 0.0 && color < 400) {return "#FEA9BD"}
-        if (color > 400.0 && color < 800) {return "#FE7F9C"}
-        if (color > 800.0) {return "#B95D72"}
-  };
+    EUcountries.forEach(function (euCountry) {
+        country_dict[euCountry] = {
+            Total: data["EU"].Total,
+            Genres: data["EU"]["Genres"]
+        }
+    });
 
-  var dataset = {};
+    NAcountries.forEach(function (naCountry) {
+        country_dict[naCountry] = {
+            Total: data["USA"].Total,
+            Genres: data["USA"]["Genres"]
+        }
+    });
 
-  Object.keys(data).forEach(function (a) {
-    Object.keys(data[a]).forEach(function (k) {
-      dataset[k] = {
-        Total: data[a][k].Total,
-        Genres: data[a][k].Genres,
-        fillColor: paletteScale(data[a][k].Total)
-        };
-    })
-  });
+    Othercountries.forEach(function (othercountries) {
+        country_dict[othercountries] = {
+            Total: data["Other"].Total,
+            Genres: data["Other"]["Genres"]
+        }
+    });
 
-  // console.log(dataset);
+    country_dict["JPN"] = {
+        Total: data["JPN"].Total,
+        Genres: data["JPN"]["Genres"]
+    };
 
-  var map = new Datamap({
-    element: document.getElementById('container'),
-    fills: {
-    defaultFill: 'grey'
-    },
-    data: dataset,
-    geographyConfig: {
-      highlightBorderWidth: 3,
-      popupTemplate: function(geography, dataset) {
-        return ["<div class='hoverinfo'>", geography.properties.name, "</br> Total revenue: ", dataset.Total, "</div>"].join('');
-      },
-    }
-  });
+    // create color palette function
+    // color can be whatever you wish
+    var paletteScale = function (color) {
+        if (color > 0 && color < 4000) {
+            return "#FEA9BD"
+        }
+        if (color > 4000 && color < 8000) {
+            return "#FE7F9C"
+        }
+        if (color > 8000) {
+            return "#B95D72"
+        }
+    };
 
-  // Object.keys(data).forEach(function (a) {
-  //   Object.keys(data[a]).forEach(function (k) {
-  //       console.log(data[a]);
-  //       if (k == "Genres") {
-  //           Object.keys(data[a][k]).forEach(function(d){
-  //               console.log(data[a][k]);
-  //               var dataa = data[a][k]
-  //               // console.log(data[a][k][d])
-  //           })
-  //       }
-  //   });
-  // });
+    Object.keys(country_dict).forEach(function (a) {
+        //console.log(country_dict[a].Total);
+        country_dict[a]["fillColor"] = paletteScale(country_dict[a].Total)
+    });
+    console.log(country_dict);
+    return country_dict
+}
 
-  var example =  data["EU"]["AUT"]["Genres"];
-  console.log(example);
+function create_map(map_data) {
 
-  var genres = [];
-  for (const [key, value] of Object.entries(example)) {
-      genres.push(value)
-  }
-  console.log(genres);
+    var map = new Datamap({
+        element: document.getElementById('container'),
+        fills: {
+            defaultFill: 'grey'
+        },
+        data: map_data,
+        geographyConfig: {
+            highlightBorderWidth: 3,
+            popupTemplate: function (geography, dataset) {
+                return ["<div class='hoverinfo'>", geography.properties.name, "</br> Total revenue: ", dataset.Total, "</div>"].join('');
+            },
+        },
+        done: function(map) {
+            map.svg.selectAll('.datamaps-subunit').on('click', function (geography) {
+                console.log(geography.id);
+                d3v5.selectAll("#barSVG").remove();
+                create_bars(map_data, geography.id);
+            });
+        }
+    });
 
+}
 
-  d3v5.select(".container-barchart").append("svg")
-                     .attr("width", 600)
-                     .attr("height", 400)
-                     .attr("id", "barSVG");
+function create_bars(map_data, country_id) {
+
+    var get_genres = map_data[country_id]["Genres"];
+
+    var genres = Object.values(get_genres);
+    console.log(genres)
+
+    d3v5.select(".container-barchart").append("svg")
+        .attr("width", 600)
+        .attr("height", 300)
+        .attr("id", "barSVG");
 
     //SVG
     var svg = d3v5.select("#barSVG");
     var h = svg.attr('height');
     var w = svg.attr('width');
 
-    // console.log(dataset);
+    var genrelist = Object.keys(get_genres);
 
-    // Scale functions
-    var xScale = d3v5.scaleLinear()
-        .domain([0, 12])
-        .range([0, w - 50]);
+    var xScale = d3v5.scaleBand()
+                    .domain(genrelist)
+                    .range([60, w ]);
 
     var yScale = d3v5.scaleLinear()
-        .domain([0, 1500])
-        .range([0, h - 50]);
+        .domain([1500, 0])
+        .range([0, h - 100]);
 
     // Define the axes
     var xAxis = d3v5.axisBottom(xScale);
+        //.text("Hallo");
 
-    // Text label for the x axis
+    // Text label for barchart
     svg.append("text")
         .attr("transform",
-            "translate(" + (w / 2) + " ," +
-            (h - 20) + ")")
+            "translate(" + (w / 2+ 50) + " ," +
+            (25) + ")")
         .style("text-anchor", "middle")
-        .text("Categories");
+        .text('Revenue per category for '+ country_id);
 
-    var yAxis = d3v5.axisLeft(yScale)
-        .tickValues([1500, 1000, 500, 0]);
+    // // Text label for the x axis
+    // svg.append("text")
+    //     .attr("transform",
+    //         "translate(" + (w / 2) + " ," +
+    //         (h - 20) + ")")
+    //     .style("text-anchor", "middle")
+    //     .text("Categories");
+
+    var yAxis = d3v5.axisLeft(yScale);
 
     // Text label for the y axis
     svg.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0)
+        .attr("y", 10)
         .attr("x", 0 - (h / 2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .text("Revenue");
 
     var bars = svg.selectAll("rect")
-        .data(genres)
+        .data(genrelist)
         .enter()
         .append("rect")
         .attr("class", "bar");
 
     var ypos = 0;
     bars.attr("x", function (d, i) {
-        return (xScale(i) + 40);
+        return xScale(d);
     })
-        .attr("y", function (e, i) {
-            var barheight = yScale(e);
+        .attr("y", function (d, i) {
+            var barheight = yScale(get_genres[d]);
             ypos = h - barheight;
-        return ypos - 50;
-    })
-        .attr("width", 46)
+            return ypos - 50;
+        })
+        .attr("width", xScale.bandwidth())
         .attr("fill", "pink")
         .attr("stroke", 'black')
-        .attr("height", function (f) {
-        return yScale(f)});
+        .attr("height", function (d) {
+            console.log(d);
+            console.log(get_genres[d]);
+            console.log(yScale(get_genres[d]));
+            return yScale(get_genres[d])
+        });
 
     // Draw axes
-    svg.append("g").attr("transform", "translate(40, " + (h - 50) + ")").call(xAxis);
-    svg.append("g").attr("transform", "translate(40, " + (0) + ")").call(yAxis);
+    svg.append("g").attr("transform", "translate(0, " + (h - 50) + ")").call(xAxis);
+    svg.append("g").attr("transform", "translate(60, " + (50) + ")").call(yAxis);
 
-});
+}
